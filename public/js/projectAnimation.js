3981a23980df85de9a1362efc10542dd23c9a890
WebFaker <nicolas.seyha@gmail.com>
2018-10-31 00:03:21 +0100

    Added back to top function for project page

--- public/js/projectAnimation.js
+++ public/js/projectAnimation.js
@@ -1,4 +1,5 @@
 $('body').css('overflow', 'hidden');
+$('.header_scrollbar_container').css('opacity','0')
 setTimeout(function(){
     $('#header_presentation').css('transform', 'translate(0,-50%)');
     $('#header_presentation').css('left', '5%');
@@ -7,4 +8,5 @@ setTimeout(function(){
 }, 1000);
 setTimeout(function(){
     $('body').css('overflow', 'auto');
+    $('.header_scrollbar_container').css('opacity','1')
 }, 1300);
